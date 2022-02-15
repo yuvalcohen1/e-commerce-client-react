ca1cbd241852d209c6cf236090a0fdbf61709697
yuval <yuvalc11499@gmail.com>
2022-02-15 17:18:38 +0200

    add HomePage component and implement it on router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, { FC } from 'react';
-import './App.css';
+import React, { FC } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
 const App: FC = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface Props {}
+
+const HomePage: FC<Props> = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
